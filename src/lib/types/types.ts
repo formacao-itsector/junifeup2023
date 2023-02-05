a497121c61c3fdedf87b1c25613aedc86f089584
João Barreto <joao.barreto@itsector.pt>
2023-02-05 22:47:39 +0000

    eslint / prettier fixes

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -2,10 +2,10 @@
 export type Anything = any;
 
 export enum Paths {
-    root = '/',
-    reactBasics = 'react-basics',
-    reactTypescript = 'react-typescript',
-    reactHooks = 'react-hooks',
-    reactState = 'react-state',
-    reactAdvanced = 'react-advanced'
+  root = '/',
+  reactBasics = 'react-basics',
+  reactTypescript = 'react-typescript',
+  reactHooks = 'react-hooks',
+  reactState = 'react-state',
+  reactAdvanced = 'react-advanced',
 }
--- src/ui/screens/reactState/reactState.tsx
+++ src/ui/screens/reactState/reactState.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export const ReactState: React.FC = () => {
-    return (
-        <div className="flex flex-col items-center gap-16 mb-40">
-            <div className="mx-2 md:mx-0">
-                REACT State
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center gap-16 mb-40">
+      <div className="mx-2 md:mx-0">REACT State</div>
+    </div>
+  );
 };
